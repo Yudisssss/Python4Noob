864ea9868e453fa09b1fe39b115b2850a27805fb
Yudisssss <xaulintobi@gmail.com>
2025-05-29 12:30:21 -0300

    Variaveis Basicas em Python

---
+++ BasicVar.py
@@ -0,0 +1,17 @@
+a = 1 #Type: int
+b = 3.14 #Type: float
+c = 'Hello, World!' #Type: str
+d = True or False #Type: bool
+e = [1, 2, 3, 4, 5] #Type: array
+f = (1, 2, 3, 4, 5) #Type: tuple
+g = {'name': 'Alice', 'age': 30, 'alive': True} #Type: dict
+#NOTE: The type tuple is different from Array because it cannot be changed after being declared
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+
