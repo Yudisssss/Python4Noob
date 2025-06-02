e59d4e48dec5bcb119c11dde69a2f4bcf5b40481
Yudisssss <xaulintobi@gmail.com>
2025-06-02 11:52:39 -0300

    Sintaxe String

---
+++ 03_Strings.py
@@ -0,0 +1,12 @@
+nome = "Flávio"
+sobrenome = "Silva"
+idade = "19"
+
+print("========================|Strings|=========================")
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Sintaxe completa\nMeu nome é " + nome + " " + sobrenome + " e tenho " + idade + " anos de idade.")
+
+print("========================|Strings Format|=========================")
+print(f"Meu nome é {nome} {sobrenome} e tenho {idade} anos de idade.")
