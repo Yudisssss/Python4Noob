23ce740d793d18711f9afe7ab1d460332214f820
Yudisssss <xaulintobi@gmail.com>
2025-06-02 11:34:07 -0300

    Operadores Lógicos

---
+++ 02_Operadores.py
@@ -0,0 +1,51 @@
+x = 5
+y = 10
+
+soma = x + y
+subtracao = x - y
+multiplicacao = x * y
+divisao = x / y
+modulo = x % y
+
+print("========================|Operações|=========================")
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
+print("Módulo:", modulo)
+#---------------------------------------------------------------------------------------------
+
+print("========================|Operações De Comparação|=========================")
+a = 1
+b = 2
+print("a é igual a b?", a == b)
+print("a é diferente de b?", a != b)
+print("a é maior que b?", a > b)
+print("a é menor que b?", a < b)
+print("a é maior ou igual a b?", a >= b)
+print("a é menor ou igual a b?", a <= b)
+#---------------------------------------------------------------------------------------------
+
+print("========================|Operações De Atibuição|=========================")
+numero = 1
+numero += 1
+print("Número:", numero)
+numero = 1
+numero -= 1
+print("Número:", numero)
+numero = 1
+numero *= 2
+print("Número:", numero)
+numero = 1
+numero /= 2
+print("Número:", numero)  
+numero = 1
+numero %= 2
+print("Número:", numero)    
+#---------------------------------------------------------------------------------------------
+
+print("========================|Operações Lógicos|=========================")
+print("O && é equivalente ao and em Python")
+print("O || é equivalente ao or em Python")
+print("O ! é equivalente ao not em Python")
+#---------------------------------------------------------------------------------------------
