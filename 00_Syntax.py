4f3d0ad5c44954ea05bcec28ddb0b98ad96e3f46
Yudisssss <xaulintobi@gmail.com>
2025-06-04 09:53:21 -0300

    Syntax basic in Python

---
+++ 00_Syntax.py
@@ -0,0 +1,12 @@
+# Python tem uma sintaxe que é um pouco complexo no começo!
+
+
+# Sintaxe correta 
+x = 1
+if x > 0:
+    print("X é maior que 0")
+
+# Sintaxe Incorreta    
+
+#if x >0:
+#print("X é maior que 0")
