16da52aebe594559e80633fbb8d89f9dc7bdf18c
Yudisssss <xaulintobi@gmail.com>
2025-06-04 10:33:41 -0300

    If Else in Pyhton

---
+++ 05_if_else.py
@@ -0,0 +1,23 @@
+a = 1
+b = 2
+print("========================|IF|=========================")
+if b > a:
+    print("B é maior que A")
+
+
+
+
+print("======================|IF ELSE|======================")
+if a > b:
+    print("A é maior que B")
+else:
+    print("B é maior que A")    
+
+
+print("=======================|ELIF|========================")    
+if a > b:
+    print("A é maior que B")
+elif a == 1:
+    print("A é igual a 1")
+else:
+    print("O que sobrou!")        
