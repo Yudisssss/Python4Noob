0c144afc15d545f4dc4b8a94eff5926d64176ea1
Yudisssss <xaulintobi@gmail.com>
2025-06-04 09:03:37 -0300

    Arrays in Python

---
+++ 04_Arrays.py
@@ -0,0 +1,41 @@
+print("=============================|Array|=============================")
+cars = ["Scort", "Monza", "Celta"]
+print(cars)
+print(cars[0])
+print(cars[1])
+print(cars[2])
+
+print("===========================|Len Array|===========================")
+y = [1,2,3,4,5,6,7,8,9]
+
+print(len(y))
+
+
+
+print("=========================|For with Array|========================")
+for i in cars:
+  print(i)
+
+
+
+
+print("===========================|Array POP|===========================")
+cars.pop()
+print(cars)
+# Add one more element to the array
+
+
+
+
+print("==========================|Array APPEND|=========================")
+cars.append("Civic")
+print(cars)
+# Delete the element of the index array
+
+
+
+
+print("==========================|Array REMOVE|=========================")
+cars.remove('Scort')
+print(cars)
+# Delete the element that has the value
